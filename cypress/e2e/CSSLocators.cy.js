6357d888d6a2cc5d2a73b910dc3b1058891fb34e
Joshua Erhahon <joshuae16@.com>
2024-04-19 16:03:10 +0100

    testing upload

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -10,7 +10,9 @@ it("csslocators", () => {
 
     cy.get('button[type="submit"].button-1.search-box-button').click()
     cy.contains('a', 'HTC One M8 Android L 5.0 Lollipop').click()  //CHatgpt help (revise)
-    cy.get(".price-value-18").contains("$245.00") //This is to check that the page....Assertion
+    cy.get(".price-value-18").contains("$245.00") //This is to check that the page....Assertion    
+
+    
     
 
 
